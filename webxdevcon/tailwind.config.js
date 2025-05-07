a8d53eebbe9899be48130f6c9aa86c88778366cb
shreyas kadlimatti <shreyas.kadlimatti9@gmail.com>
2025-05-07 23:30:30 +0530

    Delete webxdevcon directory

--- webxdevcon/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  prefix: 'tw-', // <-- This avoids class name conflicts
-  content: ["./dist/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
